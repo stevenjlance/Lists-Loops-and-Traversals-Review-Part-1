e411399a09eeed3bd11ecd03f6ce7c5f96310288
Steven Lance <slance@democracyprep.org>
2021-01-03 19:04:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+LISTS, LOOPS, AND TRAVERSALS REVIEW PART 2  
+=============
+![](https://media.giphy.com/media/J342sjcgjiqRLoFnQS/giphy.gif)  
+
+For the first part of today's class, we are going to review some key components of arrays and how to iterate through arrays using a `for` loop.
+
+GOAL
+---------
+Using your knowledge of arrays and for loops, complete the steps that have been outlined on the `script.js` file. For an components that you may have forgotten, use [W3Schools](https://www.w3schools.com/js/js_arrays.asp) and other reference materials in order to complete to expectations.
+
+TASKS FOR TODAY
+---------------
+1. There is a list of 99 names. Print out the 10th item on this list.  
+2. Print out the length of the list (it should be 99).  
+3. An empty list called someNames has been created below. Using a built in method, add 5 names from the manyNames list to the someNames list (you can use random numbers or hard code a value).  
+4. Write a for loop that prints out the name of each person.  
+5. Now iterate over the list and print out only the names which have more than 6 letters in them. Store these names in a list called `longNames`. 
+6. Iterate over the list and find the longest name in the whole thing. Then print out "The longest name in the list is ______. It has ___ letters in it."  
+7. Make a second list called `shortNames` that contains all names that are 6 letters or shorter.  
+8. Print out the `longNames` and `shortNames` lists.  
+9. Are there more long names or short names? Write code to figure that out.  
+10. Iterate over them all and print out only the names that include the letter "a". Store these names in a list called `namesWithA`. NOTE: You can check if a value includes a letter using the `.includes()` method. Be careful about the difference between a and A when checking if a character is present.  
---
+++ script.js
@@ -0,0 +1,49 @@
+var manyNames = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek","Jemima","Jackson","Keegan","Graham","Melissa","Jeanette","Grant","Kirsten","Naida","Brody","Ishmael","Kane","Seth","Rae","Eagan","Camille","Alana","Vance","Melinda","Tarik","Risa","Jordan","Camilla","Karly","Baker","Adena","Calvin","Kendall","Nasim","Kellie","Dana","Rhoda","Linus","Tyler","Ahmed","Dante","Shay","Lael","Tana","Claudia","Chadwick","Tara","Fulton","Justine","Malcolm","Rowan","Christopher","Ciaran","Ivan","Hiram","Blake","Colton","Nathaniel","Moses","Cynthia","George","Ignacia","Chanda","Wyatt","Amethyst","Vladimir","Adam","Boris","Joseph","Scarlett","Kieran","Curran","Dalton","Paul","Phillip","Plato","Renee","Natalie","Barbara","Keiko","Oleg","Xerxes","Caesar","Kareem","Ahmed","Charles","Cyrus","Adria","Winifred","Pandora","Wynne","Simon","Wanda","Coby","Nolan","Marsden","Courtney"]
+
+// 1. Above is a list of 99 names. Print out the 10th item on this list
+
+
+// 2. Print out the length of the list (it should be 99).
+
+
+// 3. An empty list called someNames has been created below. Using a built in method, add 5 names from the manyNames list to the someNames list (you can use random numbers or hard code a value).
+var someNames = [];
+
+
+
+// 4. Write a for loop that prints out the name of each person.
+
+
+
+
+// 5. Now iterate over the list and print out only the names which have more than 6 letters in them. Store these names in a list called longNames
+
+
+
+
+// 6. Iterate over the list and find the longest name in the whole thing. 
+// Then print out "The longest name in the list is ______. It has ___ letters in it."
+
+
+
+
+
+// 7. Make a second list called shortNames that contains all names that are 6 letters or shorter
+
+
+
+
+
+
+// 8. Print out the longNames and shortNames lists.
+
+
+
+
+// 9. Are there more long names or short names? Write code to figure that out.
+
+
+
+
+
+// 10. Iterate over them all and print out only the names that include the letter "a". Store these names in a list called namesWithA. NOTE: You can check if a value inclues a letter using the .includes method. Be careful about the difference between a and A when checking if a character is present.
